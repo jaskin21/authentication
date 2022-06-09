dfb5d1c10e48c2b450372a04940588a4031d192f
jaskin <jaskin.agmata@gmail.com>
2022-06-10 00:53:30 +0800

    feat: added hash password bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
 
 // validation
 import { registerValidation } from '../validation/userValidation.js';
@@ -11,9 +12,18 @@ router.post('/register', async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  //Checking if email is unique
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exist');
+
+  //Hash Pasword
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(req.body.password, salt);
+
+  //Create a new user
   const UserDetails = new User({
     email: req.body.email,
-    password: req.body.password,
+    password: hash,
     lastname: req.body.lastname,
     firstname: req.body.firstname,
     userclass: req.body.userclass,
@@ -21,9 +31,9 @@ router.post('/register', async (req, res) => {
   });
   try {
     const saveUser = await UserDetails.save();
-    res.status(200).json(saveUser);
+    res.status(200).send({ user: saveUser._id });
   } catch (err) {
-    res.status(400).json({
+    res.status(400).send({
       message: err?.message ?? 'Something went wrong, please try again',
     });
   }
